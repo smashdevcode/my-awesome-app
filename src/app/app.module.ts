1b1906407e473d161d112d1422eb50aa9b6b0b7c
James Churchill <james@smashdev.com>
2017-12-13 17:46:46 -0800

    Added new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MyTestComponent } from './my-test/my-test.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyTestComponent
   ],
   imports: [
     BrowserModule
